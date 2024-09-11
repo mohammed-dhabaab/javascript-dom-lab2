131c516bcf8f4a150e1698bf54aa4d654c5d8c7f
mohammed-dhabaab <mohammed.dhabaab@gmail.com>
2024-09-11 14:01:29 +0300

    Completed

--- main.js
+++ main.js
@@ -2,20 +2,30 @@ const cube = document.querySelector(".cube");
 
 let positionX = 0
 let positionY = 0
+let colors = [  
+    "Green",  
+    "Blue",  
+    "Cyan",  
+    "Magenta",  
+    "Orange",  
+    "Purple",  
+    "Pink",  
+    "Lime"  
+];
+let fontWeights = ["400", "500", "600", "700", "800"]
 
-setInterval(moveTheCube, 2000);
-
-let colors = ["blue", "green", "pink", "black"]
 
 function moveTheCube() {
-    cube.style.backgroundColor = colors[getRandomInt(0, 2)]
+    cube.style.backgroundColor = colors[getRandomInt(0, colors.length - 1)]
+    cube.style.fontWeight = fontWeights[getRandomInt(0, fontWeights.length - 1)]
+    cube.style.color = colors[getRandomInt(0, colors.length - 1)]
     
-    positionX += getRandomInt(1, 200);  
-    positionY += getRandomInt(1, 20);  
+    positionX = getRandomInt(1, 200);  
+    positionY = getRandomInt(1, 200);  
     cube.style.transform = `translate(${positionX}px, ${positionY}px)`;
 
-    cube.style.width = getRandomInt(100, 400) + "px"
-    cube.style.height = getRandomInt(100, 400) + "px"
+    cube.style.width = getRandomInt(100, 300) + "px"
+    cube.style.height = getRandomInt(100, 300) + "px"
 }
 
 
@@ -23,3 +33,5 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;  
 }  
 
+
+setInterval(moveTheCube, 2000);
